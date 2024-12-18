51f754b559521e981305d218bc3fab5198883013
teresamili <443776417@qq.com>
2024-12-18 17:51:31 +0800

    projects.html分页

--- views/project.py
+++ views/project.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, request
 from models import Project
 
 project_blueprint = Blueprint('projects', __name__)
@@ -6,7 +6,17 @@ project_blueprint = Blueprint('projects', __name__)
 @project_blueprint.route('/projects')
 def project_list():
     if 'username' in session:
-        project_list = Project.query.all()
-        return render_template('projects.html', projects=project_list)
+        page = request.args.get('page', 1, type=int)  # 获取页码参数，默认为第1页
+        per_page = 10  # 每页显示10条数据
+        
+        # 添加分页查询
+        pagination = Project.query.paginate(page=page, per_page=per_page, error_out=False)
+        
+        return render_template(
+            'projects.html',
+            projects=pagination.items,  # 当前页的数据
+            pagination=pagination       # 传递pagination对象到模板
+        )
     else:
         return redirect(url_for('auth.login'))
+
